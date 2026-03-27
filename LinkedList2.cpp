a2e435a5d9818c66b8584199bce6faf9c7225b72
Atharva Haibatti <aatharva357@gmail.com>
2026-03-27 11:11:41 +0530

    Create LinkedList2.cpp
    //Linked list program for student information and a multiplication table!

---
+++ LinkedList2.cpp
@@ -0,0 +1,280 @@
+//Linked list program for student information and a multiplication table!
+#include <iostream>
+#include <string>
+#include <limits>
+
+class Student {
+    public:
+    int rollno;
+    std::string name;
+    int age;
+    float m1;
+    float m2;
+    float m3;
+
+    Student* next;
+};
+
+int main () {
+    Student* head = nullptr;
+    Student* temp = nullptr;
+    int choice;
+
+    do {
+        std::cout <<"\n---- Student Information ----\n";
+        std::cout <<"1.Insert details at beginning\n";
+        std::cout <<"2.Insert details at end\n";
+        std::cout <<"3.Insert details at position\n";
+        std::cout <<"4.Delete details at beginning\n";
+        std::cout <<"5.Delete details at end\n";
+        std::cout <<"6.Delete details at position\n";
+        std::cout <<"7.Search detail\n";
+        std::cout <<"8.Display details\n";
+        std::cout <<"9.Multiplication table\n";
+        std::cout <<"10.Exit\n";
+        std::cout <<"Enter choice : ";
+        std::cin >>choice;
+
+        int rollno;
+        std::string name;
+        int age;
+        float m1;
+        float m2;
+        float m3;
+
+        if (choice == 1) {
+            std::cin.ignore(std::numeric_limits<std::streamsize> ::max(),'\n');
+
+            std::cout <<"Enter name : ";
+            std::getline (std::cin , name);
+
+            std::cout <<"Enter roll no : ";
+            std::cin >>rollno;
+
+            std::cout <<"Enter age : ";
+            std::cin >>age;
+
+            std::cout <<"Enter marks in Python programming : ";
+            std::cin >>m1;
+
+            std::cout <<"Enter marks in C++ programming : ";
+            std::cin >>m2;
+
+            std::cout <<"Enter marks in C programming : ";
+            std::cin>>m3;
+
+            Student* newNode = new Student();
+            newNode->name = name;
+            newNode->rollno = rollno;
+            newNode->age = age;
+            newNode->m1 = m1;
+            newNode->m2 = m2;
+            newNode->m3 = m3;
+            newNode->next = head;
+            head = newNode;
+        }
+
+        else if (choice == 2) {
+            std::cin.ignore(std::numeric_limits<std::streamsize> ::max(),'\n');
+
+            std::cout <<"Enter name : ";
+            std::getline (std::cin , name);
+
+            std::cout <<"Enter roll no : ";
+            std::cin >>rollno;
+
+            std::cout <<"Enter age : ";
+            std::cin >>age;
+
+            std::cout <<"Enter marks in Python programming : ";
+            std::cin >>m1;
+
+            std::cout <<"Enter marks in C++ programming : ";
+            std::cin >>m2;
+
+            std::cout <<"Enter marks in C programming : ";
+            std::cin>>m3;
+
+            Student* newNode = new Student();
+            newNode->name = name;
+            newNode->rollno = rollno;
+            newNode->age = age;
+            newNode->m1 = m1;
+            newNode->m2 = m2;
+            newNode->m3 = m3;
+            newNode->next = nullptr;
+
+            if (head == nullptr) {
+                head = newNode;
+            } else {
+                Student* t = head;
+                while (t->next != nullptr) {
+                    t = t->next;
+                }
+                t->next = newNode;
+            }
+        }
+
+        else if (choice == 3) {
+            int pos;
+            std::cout <<"Enter position to insert details : ";
+            std::cin >>pos;
+
+            std::cin.ignore(std::numeric_limits<std::streamsize> ::max(),'\n');
+
+            std::cout <<"Enter name : ";
+            std::getline (std::cin , name);
+
+            std::cout <<"Enter roll no : ";
+            std::cin >>rollno;
+
+            std::cout <<"Enter age : ";
+            std::cin >>age;
+
+            std::cout <<"Enter marks in Python programming : ";
+            std::cin >>m1;
+
+            std::cout <<"Enter marks in C++ programming : ";
+            std::cin >>m2;
+
+            std::cout <<"Enter marks in C programming : ";
+            std::cin>>m3;
+
+            Student* newNode = new Student();
+            newNode->name = name;
+            newNode->rollno = rollno;
+            newNode->age = age;
+            newNode->m1 = m1;
+            newNode->m2 = m2;
+            newNode->m3 = m3;
+
+            if (pos == 1) {
+                newNode->next = head;
+                head = newNode;
+            } else {
+                temp = head;
+
+                for (int i = 1; i < pos - 1; i++) {
+                    temp = temp->next;
+                }
+                newNode->next = temp->next;
+                temp->next = newNode;
+            }
+        }
+
+        else if (choice == 4) {
+            if (head == nullptr) {
+                std::cout <<"\nList is empty!";
+            } else {
+                Student* del = head;
+                head = head->next;
+                delete del;
+                std::cout <<"\nFirst details deleted";
+            }
+        }
+
+        else if (choice == 5) {
+            if (head == nullptr) {
+                std::cout <<"\nList is empty!";
+            } else if (head->next == nullptr) {
+                delete head;
+                head = nullptr;
+                std::cout <<"\nOnly details deleted";
+            } else {
+                temp = head;
+
+                while (temp->next->next != nullptr) {
+                    temp = temp->next;
+                }
+                Student* del = temp->next;
+                temp->next = nullptr;
+                delete del;
+                std::cout <<"\nLast details deleted";
+            }
+        }
+
+        else if (choice == 6) {
+            int pos;
+            std::cout <<"Enter a position to delete : ";
+            std::cin >>pos;
+
+            if (head == nullptr) {
+                std::cout <<"\nList is empty!";
+            } else if (pos == 1) {
+                temp = head;
+                head = head->next;
+                delete temp;
+            } else {
+                temp = head;
+
+                for (int i = 1; i < pos - 1; i++) {
+                    temp = temp->next;
+                }
+                Student* nodetoDelete = temp->next;
+                temp->next = nodetoDelete->next;
+                delete nodetoDelete;
+            }
+        }
+
+        else if (choice == 7) {
+            int key;
+            bool found = false;
+
+            std::cout <<"Enter Roll No to get details : ";
+            std::cin >>key;
+
+            temp = head;
+
+            if (head == nullptr) {
+                std::cout <<"\nList is empty!";
+            } else {
+                while (temp != nullptr) {
+                    if (temp->rollno == key) {
+                        std::cout <<"\nDetails Found : \n";
+                        std::cout <<"Name : "<<temp->name<<" | Roll No : "<<temp->rollno<<" | Age : "<<temp->age<<" | Total Marks : "<<temp->m1+temp->m2+temp->m3<<"\n";
+                        temp = temp ->next;
+                        found = true;
+                        break;
+                    }
+                    temp = temp->next;
+                }
+                if (!found) {
+                    std::cout <<"\nDetails not found!";
+                }
+            }
+        }
+
+        else if (choice == 8) {
+            if (head == nullptr) {
+                std::cout <<"\nList is empty!";
+            } else {
+                std::cout <<"\n---- Student Details ----\n"; 
+                temp = head;
+                while (temp != nullptr) {
+                    std::cout <<"Name : "<<temp->name<<" | Roll No : "<<temp->rollno<<" | Age : "<<temp->age<<" | Total Marks : "<<temp->m1+temp->m2+temp->m3<<"\n";
+                    temp = temp ->next;
+                }
+            }
+        }
+
+        else if (choice == 9) {
+            int n;
+            int limit;
+
+            std::cout <<"\nEnter a number : ";
+            std::cin >>n;
+
+            std::cout <<"Enter how many ?: ";
+            std::cin >>limit;
+
+            for (int i = 1; i <= limit; i++) {
+                std::cout <<n<<" X "<<i<<" = "<<n * i<<"\n";
+            }
+        }
+
+        else if (choice == 10) {
+            std::cout <<"\nProgram Exited!";
+        }
+
+    } while (choice != 10);
+}
